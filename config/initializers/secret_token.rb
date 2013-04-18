0f7ca370a4cec42229da58016881787f8c983e03
yu kawase <kawase@kawase-no-MacBook-Air.local>
2013-04-18 14:48:06 +0900

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fanap::Application.config.secret_token = '4c3d184dc03b68785be24e2dd27bdf616136f780e3af099e816d2ccbd5002643372c4772c4f6995a915ec14794e3194ee85266343b5d79666d520fb327be71b5'
